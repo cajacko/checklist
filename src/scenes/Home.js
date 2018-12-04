daaf208776ababad37a184f570140ca8a3357eaf
Charlie Jackson <charlie@charliejackson.com>
2018-12-04 14:18:59 +0000

    ✨ feat(list): Add the "Add new item" button to the list
    - Original Branch: init

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import HeaderWithContent from '@cajacko/lib/components/Layout/HeaderWithContent';
 import CardsList from '@cajacko/lib/components/Cards/List';
+import { PLUS } from '@cajacko/lib/config/icons';
 
 /**
  * The home scene
@@ -13,7 +14,15 @@ const SceneHome = () => (
       title: 'General.Header',
     }}
   >
-    <CardsList cards={[{ text: { _textFromConst: 'Hello' } }]} />
+    <CardsList
+      cards={[{ text: { _textFromConst: 'Example List Item' }, key: 'yay' }]}
+      bottomItem={{
+        text: 'General.NewItem',
+        leftIcon: PLUS,
+        action: () => console.log('Add new item'),
+        greyedOut: true,
+      }}
+    />
   </HeaderWithContent>
 );
 
