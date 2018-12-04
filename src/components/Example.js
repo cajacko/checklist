9cdda3cea1b20b1a57bfc24b3a4fe87bfbd0f0ea
Charlie Jackson <charlie@charliejackson.com>
2018-12-04 11:50:26 +0000

    ✨ feat(header): Add the static header
    - Original Branch: init

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,8 +1,17 @@
 // @flow
 
 import React from 'react';
+import HeaderWithContent from '@cajacko/lib/components/Layout/HeaderWithContent';
 import { Span } from '@cajacko/lib/components/UI';
 
-const Example = () => <Span>Example</Span>;
+const Example = () => (
+  <HeaderWithContent
+    header={{
+      title: 'General.Header',
+    }}
+  >
+    <Span>Example</Span>
+  </HeaderWithContent>
+);
 
 export default Example;
